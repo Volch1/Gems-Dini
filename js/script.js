ef8d09d6453c420dbbc5079190f476bc2a138d1b
Volch Ivan Vladimirovich <101183285+Volch1@users.noreply.github.com>
2022-08-25 14:24:55 +0300

    sa

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+// let start = document.querySelector('.start');
+// prompt('Введіть своє імя');
+
+const dino = document.querySelector('#dino');
+const cactus = document.querySelector('#cactus');
+
+document.addEventListener('keydown', function(e){
+    jump();
+});
+
+function jump (){
+  if(dino.classList != "jump"){
+    dino.classList.add('jump')
+  } 
+  setTimeout(function(){
+    dino.classList.remove('jump')
+  }, 300)
+}
+
+let isAlive = setInterval(() => {
+   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top')) 
+   let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left')) 
+
+   if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+    alert('GAME OVER!!');
+   }
+}, 10);
+
